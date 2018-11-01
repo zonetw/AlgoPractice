34cd6c54fc9e37436e5137b657ea5727b0f2f7ec
zonetw <zonetw@users.noreply.github.com>
2018-11-01 23:13:40 +0800

    add capitalize solution 2

--- src/capitalization/index.js
+++ src/capitalization/index.js
@@ -1,10 +1,26 @@
-//solution 1
+//solution 1: split sentence into words by space and deal with them separately
+/*
 function capitalize(str) {
   let wordsArr = str.split(" ").map(word => {
     return word[0].toUpperCase() + word.slice(1);
   });
 
   return wordsArr.join(" ");
+}
+*/
+
+// solution 2: recognize words by space, but change them char by char
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
 }
 
 module.exports = capitalize;
